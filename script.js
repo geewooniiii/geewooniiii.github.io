a046380842b6cdf9348aee6e38f2667f78026d37
unknown <numbawon12345@gmail.com>
2024-09-15 11:10:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get all buttons and add event listeners
     const buttons = document.querySelectorAll("button");
 
     buttons.forEach(button => {
         button.addEventListener("click", function() {
-            // Navigate to the page based on data-link attribute
             window.location.href = this.getAttribute("data-link");
         });
     });
